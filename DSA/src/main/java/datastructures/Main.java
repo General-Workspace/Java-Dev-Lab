688bf89f186aae599824a4217452dc112427e0b5
aniebietafia <aniebietafia87@gmail.com>
2024-11-06 23:58:27 +0100

    setting up dev environment

---
+++ DSA/src/main/java/datastructures/Main.java
@@ -0,0 +1,10 @@
+package datastructures;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide the name of the data structure to test as a command line argument.");
+            System.out.println("Available data structures: stack, queue, linkedlist, binarytree, binarysearchtree, heap, graph");
+        }
+    }
+}
