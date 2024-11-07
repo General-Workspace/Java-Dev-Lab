46052e366177783f2459aad2eb4c547594b1b6d1
aniebietafia <aniebietafia87@gmail.com>
2024-11-07 19:13:11 +0100

    dsa: Big O(n)

--- DSA/src/main/java/datastructures/Main.java
+++ DSA/src/main/java/datastructures/Main.java
@@ -1,10 +1,9 @@
 package datastructures;
 
+import static datastructures.PrintItems.printItems;
+
 public class Main {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Please provide the name of the data structure to test as a command line argument.");
-            System.out.println("Available data structures: stack, queue, linkedlist, binarytree, binarysearchtree, heap, graph");
-        }
+        printItems(10);
     }
 }
---
+++ DSA/src/main/java/datastructures/PrintItems.java
@@ -0,0 +1,9 @@
+package datastructures;
+
+public class PrintItems {
+    public static void printItems(int n) {
+        for(int i = 0; i < n; ++i) {
+            System.out.println(i);
+        }
+    }
+}
