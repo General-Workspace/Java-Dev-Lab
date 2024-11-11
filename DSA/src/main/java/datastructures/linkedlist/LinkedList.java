ba70cefe80abd7c52185b4b4e1a599e89ca91d65
aniebietafia <aniebietafia87@gmail.com>
2024-11-11 10:22:11 +0100

    linked list: remove last

--- DSA/src/main/java/datastructures/linkedlist/LinkedList.java
+++ DSA/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -5,7 +5,7 @@ public class LinkedList {
     private Node tail;
     private int length;
 
-    class Node {
+    class Node extends Throwable {
         int value;
         Node next;
 
@@ -39,6 +39,24 @@ public class LinkedList {
             tail = newNode;
         }
         length++;
+    }
+
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+        while(temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
     }
 
     public void getHead() {
--- DSA/src/main/java/datastructures/linkedlist/Main.java
+++ DSA/src/main/java/datastructures/linkedlist/Main.java
@@ -2,14 +2,31 @@ package datastructures.linkedlist;
 
 public class Main {
     public static void main(String[] args) {
-//        LinkedList myLinkedList = new LinkedList(4);
-//
-//        myLinkedList.getHead();
-//        myLinkedList.getTail();
-//        myLinkedList.getLength();
+        int count = 20;
+        String sep = "*";
+
+        System.out.println(sep.repeat(count));
+        System.out.println("Creating Node");
+        LinkedList myLinkedList1 = new LinkedList(4);
+        myLinkedList1.getHead();
+        myLinkedList1.getTail();
+        myLinkedList1.getLength();
+        System.out.println(sep.repeat(count));
+
+        System.out.println("Linked List: Append");
         LinkedList myLinkedList = new LinkedList(1);
         myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
 
         myLinkedList.printList();
+        System.out.println(sep.repeat(count));
+
+        System.out.println("Linked List: Remove");
+        System.out.println(myLinkedList.removeLast().value);
+        System.out.println(myLinkedList.removeLast().value);
+        System.out.println(myLinkedList.removeLast().value);
+        System.out.println(myLinkedList.removeLast().value);
+        System.out.println(myLinkedList.removeLast());
     }
 }
