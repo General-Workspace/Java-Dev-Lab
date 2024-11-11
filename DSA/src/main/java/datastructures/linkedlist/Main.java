cc219f5fa227568ac4504282506c709c787f157d
aniebietafia <aniebietafia87@gmail.com>
2024-11-11 09:36:59 +0100

    linked_list: append method

--- DSA/src/main/java/datastructures/linkedlist/Main.java
+++ DSA/src/main/java/datastructures/linkedlist/Main.java
@@ -2,11 +2,13 @@ package datastructures.linkedlist;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(4);
-
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
+//        LinkedList myLinkedList = new LinkedList(4);
+//
+//        myLinkedList.getHead();
+//        myLinkedList.getTail();
+//        myLinkedList.getLength();
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
 
         myLinkedList.printList();
     }
