971feb7ec15b5a95321f94880f21cf0ffe5e436b
aniebietafia <aniebietafia87@gmail.com>
2024-11-22 14:24:30 +0100

    hello_world: Printing Hello World to the console.

---
+++ java-coding-sessions/src/main/java/helloworld/CopyCodeExercise.java
@@ -0,0 +1,8 @@
+package helloworld;
+
+public class CopyCodeExercise {
+    public static void main(String[] args) {
+        System.out.println("Hello, Aniebiet");
+        System.out.println("Hello World");
+    }
+}
