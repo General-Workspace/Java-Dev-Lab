16c2dcd7b6eeae7c675ee039cf9930a00ba71105
aniebietafia <aniebietafia87@gmail.com>
2024-11-22 14:40:25 +0100

    control flow: Using 'if' statement.

---
+++ java-coding-sessions/src/main/java/control_flow/FirstClass.java
@@ -0,0 +1,12 @@
+package control_flow;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, Aniebiet");
+
+        boolean isAlien = false;
+
+        if (!isAlien)
+            System.out.println("It is not an alien!");
+    }
+}
