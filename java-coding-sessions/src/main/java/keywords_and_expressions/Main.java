c47daf1657bd5e478a134363e7f39cb38c67d858
aniebietafia <aniebietafia87@gmail.com>
2024-11-26 20:21:48 +0100

    expressions

---
+++ java-coding-sessions/src/main/java/keywords_and_expressions/Main.java
@@ -0,0 +1,14 @@
+package keywords_and_expressions;
+
+public class Main {
+    public static void main(String[] args) {
+        // expressions
+        double kilometres = (100 * 1.609344);
+
+        int highScore = 50;
+
+        if (highScore > 25) {
+            highScore = 1000 + highScore;
+        }
+    }
+}
