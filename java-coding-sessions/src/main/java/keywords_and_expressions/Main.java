b1c293b225039b07d766bf11128c309a392d73fd
aniebietafia <aniebietafia87@gmail.com>
2024-11-27 10:11:13 +0100

    if keyword challenge

--- java-coding-sessions/src/main/java/keywords_and_expressions/Main.java
+++ java-coding-sessions/src/main/java/keywords_and_expressions/Main.java
@@ -1,8 +1,10 @@
 package keywords_and_expressions;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        // expressions
+// expressions
         double kilometres = (100 * 1.609344);
 
         int highScore = 50;
@@ -10,5 +12,20 @@ public class Main {
         if (highScore > 25) {
             highScore = 1000 + highScore;
         }
+
+        String expectedOutcome = """
+                Twinkle twinkle little star
+                Up above the world so high
+                Like a diamond in the sky.
+                """;
+        System.out.println(expectedOutcome);
+        String[] collections = expectedOutcome.split("\n");
+        System.out.println(Arrays.toString(collections));
+
+        int count = 0;
+        for (int i = 0; i < collections.length; ++i) {
+            count = collections.length;
+        }
+        System.out.println(count);
     }
 }
