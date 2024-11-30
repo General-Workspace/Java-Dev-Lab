76e9602286c8047963aac8ba23db41ec749b9973
aniebietafia <aniebietafia87@gmail.com>
2024-11-30 16:39:11 +0100

    switch challenge

--- java-coding-sessions/src/main/java/control_flow/EnhancedSwitchChallenge.java
+++ java-coding-sessions/src/main/java/control_flow/EnhancedSwitchChallenge.java
@@ -3,7 +3,7 @@ package control_flow;
 public class EnhancedSwitchChallenge {
     public String printDayOfWeek(int day) {
 
-        return switch (day) {
+        String dayOfTheWeek = switch (day) {
             case 0 -> { yield "Sunday"; }
             case 1 -> { yield "Monday"; }
             case 2 -> { yield "Tuesday"; }
@@ -13,25 +13,31 @@ public class EnhancedSwitchChallenge {
             case 6 -> { yield "Saturday"; }
             default -> { yield "Invalid Day"; }
         };
+
+        return day + " stands for " + dayOfTheWeek;
     }
 
     public String printWeekDay(int day) {
+        String dayOfTheWeek;
+
         if (day == 0) {
-            return day + " is Sunday";
+            dayOfTheWeek = "Sunday";
         } else if (day == 1) {
-            return day + " is Monday";
+            dayOfTheWeek = "Monday";
         } else if (day == 2) {
-            return day + " is Tuesday";
+            dayOfTheWeek = "Tuesday";
         } else if (day == 3) {
-            return day + " is Wednesday";
+            dayOfTheWeek = "Wednesday";
         } else if (day == 4) {
-            return day + " is Thursday";
+            dayOfTheWeek = "Thursday";
         } else if (day == 5) {
-            return day + " is Friday";
+            dayOfTheWeek = "Friday";
         } else if (day == 6) {
-            return day + " is Saturday";
+            dayOfTheWeek = "Saturday";
         } else {
-            return "Invalid Day";
+            dayOfTheWeek = "Invalid Day";
         }
+
+        return dayOfTheWeek;
     }
 }
