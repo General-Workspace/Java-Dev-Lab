c0574696aae5e4c3341ef751412f58101c1a6995
aniebietafia <aniebietafia87@gmail.com>
2024-12-26 13:01:37 +0100

    print diagonal star

--- java-coding-sessions/src/main/java/udemy_exercises_2/DiagonalStar.java
+++ java-coding-sessions/src/main/java/udemy_exercises_2/DiagonalStar.java
@@ -71,4 +71,27 @@ NOTE: Do not add a main method to the solution code.
  */
 
 public class DiagonalStar {
+    public static void main(String[] args) {
+        printSquareStar(5);
+        printSquareStar(8);
+        printSquareStar(4);
+        printSquareStar(2);
+        printSquareStar(1);
+    }
+    public static void printSquareStar(int number) {
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        } else {
+            for (int row = 1; row <= number; row++) {
+                for (int column = 1; column <= number; column++) {
+                    if (row == 1 || row == number || column == 1 || column == number || row == column || column == (number - row + 1)) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
 }
