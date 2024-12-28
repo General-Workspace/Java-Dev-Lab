309684ae3fb5120b3410d233faa8c04622febdb3
aniebietafia <aniebietafia87@gmail.com>
2024-12-28 16:58:45 +0100

    flour packer

--- java-coding-sessions/src/main/java/udemy_exercises_2/FlourPacker.java
+++ java-coding-sessions/src/main/java/udemy_exercises_2/FlourPacker.java
@@ -38,4 +38,30 @@ NOTE: Do not add a main method to the solution code.
  */
 
 public class FlourPacker {
+    public static void main(String[] args) {
+        System.out.println(canPack(1, 0, 4));
+        System.out.println(canPack(1, 0, 5));
+        System.out.println(canPack(0, 5, 4));
+        System.out.println(canPack(2, 2, 11));
+        System.out.println(canPack(-3, 2, 12));
+    }
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        if (bigCount < 0 || smallCount < 0|| goal < 0) return false;
+
+        int bigKilos = bigCount * 5;
+        int smallKilos = smallCount * 1;
+
+        if (bigKilos + smallKilos < goal) return false;
+
+        if (bigKilos + smallKilos == goal) return true;
+
+        if (bigKilos + smallKilos > goal) {
+            if (bigKilos > goal) {
+                return smallKilos >= goal % 5;
+            } else {
+                return bigKilos + smallKilos >= goal;
+            }
+        }
+        return false;
+    }
 }
