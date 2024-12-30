1c6fa23907f88218d5aad9d0cd9caba3978cfd11
aniebietafia <aniebietafia87@gmail.com>
2024-12-30 15:29:18 +0100

    perfect number

--- java-coding-sessions/src/main/java/udemy_exercises_2/PerfectNumber.java
+++ java-coding-sessions/src/main/java/udemy_exercises_2/PerfectNumber.java
@@ -42,4 +42,22 @@ NOTE: Do not add a main method to the solution code.
  */
 
 public class PerfectNumber {
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(6)); // true
+        System.out.println(isPerfectNumber(28)); // true
+        System.out.println(isPerfectNumber(5)); // false
+        System.out.println(isPerfectNumber(-1)); // false
+    }
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == number;
+    }
 }
