f98f32a12789c363c1ef25120e0e7fb702dc1cdc
aniebietafia <aniebietafia87@gmail.com>
2024-12-30 15:27:02 +0100

    paint job

--- java-coding-sessions/src/main/java/udemy_exercises_2/PaintJob.java
+++ java-coding-sessions/src/main/java/udemy_exercises_2/PaintJob.java
@@ -86,4 +86,16 @@ NOTE: Do not add the main method to the solution code.
  */
 
 public class PaintJob {
+    public static void main(String[] args) {
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5, 2)); // -1
+        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2)); // 3
+        System.out.println(getBucketCount(2.75, 3.25, 2.5, 1)); // 3
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+        return (int) Math.ceil((width * height - areaPerBucket * extraBuckets) / areaPerBucket);
+    }
 }
