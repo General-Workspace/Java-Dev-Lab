0283e27365ccfc7747b2f8dd064c471aa58752b6
aniebietafia <aniebietafia87@gmail.com>
2024-12-30 15:30:30 +0100

    shared digit

--- java-coding-sessions/src/main/java/udemy_exercises_2/SharedDigit.java
+++ java-coding-sessions/src/main/java/udemy_exercises_2/SharedDigit.java
@@ -26,4 +26,19 @@ NOTE: Do not add a main method to the solution code.
  */
 
 public class SharedDigit {
+    public static void main(String[] args) {
+        System.out.println(hasSharedDigit(12, 23)); // true
+        System.out.println(hasSharedDigit(9, 99)); // false
+        System.out.println(hasSharedDigit(15, 55)); // true
+    }
+    public static boolean hasSharedDigit(int num1, int num2) {
+        if ((num1 < 10 || num1 > 99) || (num2 < 10 || num2 > 99)) {
+            return false;
+        }
+        int num1FirstDigit = num1 / 10;
+        int num1SecondDigit = num1 % 10;
+        int num2FirstDigit = num2 / 10;
+        int num2SecondDigit = num2 % 10;
+        return num1FirstDigit == num2FirstDigit || num1FirstDigit == num2SecondDigit || num1SecondDigit == num2FirstDigit || num1SecondDigit == num2SecondDigit;
+    }
 }
