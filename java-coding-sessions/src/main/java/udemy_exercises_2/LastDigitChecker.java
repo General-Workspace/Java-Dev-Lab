cfd09c0723a839fe2aa5ed92cde7244892b6168f
aniebietafia <aniebietafia87@gmail.com>
2024-12-30 14:57:07 +0100

    last digit checker

--- java-coding-sessions/src/main/java/udemy_exercises_2/LastDigitChecker.java
+++ java-coding-sessions/src/main/java/udemy_exercises_2/LastDigitChecker.java
@@ -40,4 +40,22 @@ NOTE: Do not add a main method to the solution code.
  */
 
 public class LastDigitChecker {
+    public static void main(String[] args) {
+        System.out.println(hasSameLastDigit(41, 22, 71)); // true
+        System.out.println(hasSameLastDigit(23, 32, 42)); // true
+        System.out.println(hasSameLastDigit(9, 99, 999)); // false
+    }
+    public static boolean hasSameLastDigit(int num1, int num2, int num3) {
+        if (isValid(num1) || isValid(num2) || isValid(num3)) {
+            return false;
+        }
+        int lastDigit1 = num1 % 10;
+        int lastDigit2 = num2 % 10;
+        int lastDigit3 = num3 % 10;
+        return lastDigit1 == lastDigit2 || lastDigit1 == lastDigit3 || lastDigit2 == lastDigit3;
+    }
+
+    public static boolean isValid(int number) {
+        return number < 10 || number > 1000;
+    }
 }
