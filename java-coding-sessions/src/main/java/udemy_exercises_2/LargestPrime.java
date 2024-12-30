676a1b78ba7e9b0c431d582b5c4fb590073b25b9
aniebietafia <aniebietafia87@gmail.com>
2024-12-30 14:53:07 +0100

    largest prime

--- java-coding-sessions/src/main/java/udemy_exercises_2/LargestPrime.java
+++ java-coding-sessions/src/main/java/udemy_exercises_2/LargestPrime.java
@@ -34,4 +34,32 @@ NOTE: Do not add a main method to the solution code.
  */
 
 public class LargestPrime {
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21)); // 7
+        System.out.println(getLargestPrime(217)); // 31
+        System.out.println(getLargestPrime(0)); // -1
+        System.out.println(getLargestPrime(45)); // 5
+        System.out.println(getLargestPrime(-1)); // -1
+    }
+    public static int getLargestPrime(int number) {
+        if (number <= 1) {
+            return -1;
+        }
+        int largestPrime = 0;
+        for (int i = 2; i <= number; i++) {
+            if (number % i == 0) {
+                boolean isPrime = true;
+                for (int j = 2; j <= i / 2; j++) {
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime) {
+                    largestPrime = i;
+                }
+            }
+        }
+        return largestPrime;
+    }
 }
