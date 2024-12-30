b3e82d8fa7abafe0e7a07d0cd7c3bd390a9b1857
aniebietafia <aniebietafia87@gmail.com>
2024-12-30 15:15:27 +0100

    number palindrome

--- java-coding-sessions/src/main/java/udemy_exercises_2/NumberPalindrome.java
+++ java-coding-sessions/src/main/java/udemy_exercises_2/NumberPalindrome.java
@@ -52,4 +52,20 @@ NOTE: The method isPalindrome needs to be defined as public static like we have 
  */
 
 public class NumberPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-1221)); // true
+        System.out.println(isPalindrome(707)); // true
+        System.out.println(isPalindrome(11212)); // false
+    }
+
+    public static boolean isPalindrome(int number) {
+        int reverse = 0;
+        int originalNumber = number;
+        while (number != 0) {
+            int lastDigit = number % 10;
+            reverse = reverse * 10 + lastDigit;
+            number /= 10;
+        }
+        return originalNumber == reverse;
+    }
 }
