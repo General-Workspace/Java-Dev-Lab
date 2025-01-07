61e08eb939328de0838ff4c2470820f8285be2f1
aniebietafia <aniebietafia87@gmail.com>
2025-01-07 13:56:36 +0100

    minimum element

--- java-coding-sessions/src/main/java/arrays/Main.java
+++ java-coding-sessions/src/main/java/arrays/Main.java
@@ -1,14 +1,28 @@
 package arrays;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        SortingArray result = new SortingArray();
-        int[] randomArrayDesc = result.getRandomArray(10);
-        System.out.println(Arrays.toString(randomArrayDesc));
+//        SortingArray result = new SortingArray();
+//        int[] randomArrayDesc = result.getRandomArray(10);
+//        System.out.println(Arrays.toString(randomArrayDesc));
+//
+//        int[] randomArrayAsc = result.getRandomArray(10, 500);
+//        System.out.println(Arrays.toString(randomArrayAsc));
 
-        int[] randomArrayAsc = result.getRandomArray(10, 500);
-        System.out.println(Arrays.toString(randomArrayAsc));
+        MinimumElement minimumElement = new MinimumElement();
+        System.out.println("Enter numbers separated by commas");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int[] numArray = minimumElement.readIntegers(input);
+        System.out.println(Arrays.toString(numArray));
+
+        System.out.println("- ".repeat(30));
+
+        int minimumNumber = minimumElement.findMin(numArray);
+        System.out.println("Minimum Number: " + minimumNumber);
+
     }
 }
---
+++ java-coding-sessions/src/main/java/arrays/MinimumElement.java
@@ -0,0 +1,23 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class MinimumElement {
+    public int[] readIntegers(String list) {
+        String[] stringList = list.split(", ");
+
+        int[] numbers = new int[stringList.length];
+        for (int i = 0; i < stringList.length; ++i) {
+            numbers[i] = Integer.parseInt(stringList[i]);
+        }
+
+//        Arrays.sort(numbers);
+        return numbers;
+    }
+
+    public int findMin(int[] input) {
+        Arrays.sort(input);
+
+        return input[0];
+    }
+}
