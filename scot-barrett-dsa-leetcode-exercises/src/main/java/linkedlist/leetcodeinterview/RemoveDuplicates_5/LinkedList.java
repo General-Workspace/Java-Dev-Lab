473eaeaacc0d11605211adc44b4d7ca8afbfcec7
aniebietafia <aniebietafia87@gmail.com>
2025-02-15 15:04:49 +0100

    remove duplicates

---
+++ scot-barrett-dsa-leetcode-exercises/src/main/java/linkedlist/leetcodeinterview/RemoveDuplicates_5/LinkedList.java
@@ -0,0 +1,90 @@
+package linkedlist.leetcodeinterview.RemoveDuplicates_5;
+
+
+public class LinkedList {
+
+    private Node head;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.printf("%d ", temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nLinked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        length++;
+    }
+
+    public void removeDuplicates() {
+        Node current = head;
+        while (current != null) {
+            Node temp = current;
+            while (temp.next != null) {
+                if (current.value == temp.next.value) {
+                    temp.next = temp.next.next;
+                    length--;
+                } else {
+                    temp = temp.next;
+                }
+            }
+            current = current.next;
+        }
+    }
+
+}
